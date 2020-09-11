89ddc9a4d8a70afef64bd31975125b7b4c9597d1
Marcelo Ramiro <marcelo.jdeveloper@outlook.com>
2020-09-11 12:48:15 -0300

    Apresentando o modelo para o usuário

--- app/js/views/NegociacoesView.js
+++ app/js/views/NegociacoesView.js
@@ -1,5 +1,28 @@
 class NegociacoesView {
+    constructor(selector) {
+        this._elemento = document.querySelector(selector);
+    }
+    update() {
+        this._elemento.innerHTML = this.template();
+    }
     template() {
-        return "";
+        return `
+            <table class=table table-hover table-bordered>
+                <thead>
+                    <tr>
+                        <th>DATA </th>
+                        <th> QUANTIDADE </th>
+                        <th> VALOR </th>
+                        <th> VOLUME </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                </tbody>
+
+                <tfoot>
+                </tfoot>
+            </table>
+            `;
     }
 }
